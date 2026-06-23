2b54701282095fb1365895cc0cdb1d4cd8328518
Lagnasha-Tripathy <tripathylagnasha@gmail.com>
2026-06-23 11:05:39 +0530

    Added task completion and local storage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,45 @@
 import { FaTrash } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+const [tasks, setTasks] = useState(() => {
+  const savedTasks = localStorage.getItem("tasks");
+  return savedTasks ? JSON.parse(savedTasks) : [];
+});
+useEffect(() => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}, [tasks]);
   const [taskInput, setTaskInput] = useState("");
 
   const addTask = () => {
-    if (!taskInput.trim()) return;
+  if (!taskInput.trim()) return;
 
-    setTasks([...tasks, taskInput]);
-    setTaskInput("");
-  };
+  setTasks([
+    ...tasks,
+    {
+      text: taskInput,
+      completed: false,
+    },
+  ]);
+
+  setTaskInput("");
+};
 
   const deleteTask = (index) => {
     setTasks(tasks.filter((_, i) => i !== index));
   };
+  const toggleTask = (index) => {
+  const updatedTasks = [...tasks];
 
+  updatedTasks[index].completed =
+    !updatedTasks[index].completed;
+
+  setTasks(updatedTasks);
+};
+  const completedTasks = tasks.filter(
+  (task) => task.completed
+).length;
+  <h3>Completed Tasks: {completedTasks}</h3>
   return (
     <div style={{ padding: "30px", maxWidth: "600px", margin: "auto" }}>
       <h1>🚀 Task Manager Pro</h1>
@@ -31,16 +55,26 @@ function App() {
       <button onClick={addTask} style={{ marginLeft: "10px" }}>
         Add Task
       </button>
-
+      <h3>Total Tasks: {tasks.length}</h3>
       <ul>
         {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
+          <li
+            key={index}
+            style={{
+            textDecoration: task.completed ? "line-through" : "none",
+  }}
+>
+  {task.text}
             <button
               onClick={() => deleteTask(index)}
               style={{ marginLeft: "10px" }}
             >
               <FaTrash /> Delete
+            </button><button
+              onClick={() => toggleTask(index)}
+              style={{ marginLeft: "10px" }}
+             >
+             {task.completed ? "Undo" : "Complete"}
             </button>
           </li>
         ))}
